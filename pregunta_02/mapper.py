9e5f2d5cd1456419048e2fcdabda06501ef65ca9
lauracamilaballesteros <92760450+lauracamilaballesteros@users.noreply.github.com>
2023-05-18 23:15:09 -0500

    pregunta2

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#
+import sys
+if __name__ == "__main__":
+    for linea in sys.stdin:
+        columnas = linea.split(",")
+        columna_purpose = columnas[3]
+        columna_amount = columnas[4]
+        sys.stdout.write(f"{columna_purpose}\t{columna_amount}\n")
+        
