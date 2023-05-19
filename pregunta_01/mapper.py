8b9adb3d5e671380732dd9ef308383e636886dd4
lauracamilaballesteros <92760450+lauracamilaballesteros@users.noreply.github.com>
2023-05-18 21:43:57 -0500

    pregunta 1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for linea in sys.stdin:
+        columnas = linea.split(",")
+        columna_credit = columnas[2]
+        sys.stdout.write(f"{columna_credit}\n")
+       
--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    atributo = {}
+    for linea in sys.stdin:
+        columnas = linea.split("\n")
+        columna_credit = columnas[0]
+        if columna_credit in atributo:
+            atributo[columna_credit] += 1
+        else:
+            atributo[columna_credit] =1
+    atributo_sort = sorted(atributo.items())
+    for atributo,valor in atributo_sort:
+        sys.stdout.write(f"{atributo}\t{valor}\n")
